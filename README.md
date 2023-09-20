b332efc0ca88f3d7af55c322a0ede7e4ee2c4753
VivianeSouto <36643965+VivianeSouto@users.noreply.github.com>
2023-09-20 16:48:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-divider
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-divider1-wr9fbw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: 'my-app',
+  selector: 'app-component',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+}
